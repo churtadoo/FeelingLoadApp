e9e41a9ecdbf58cfcd9f9ca23d3a511e681d33ff
cristina.hurtado <cristina.hurtado@birchmangroup.com>
2018-11-20 22:04:55 +0100

    Added AuthenticationService

--- app/src/main/java/edu/uoc/churtado/feelingloadapp/services/UserAuthenticationService.java
+++ app/src/main/java/edu/uoc/churtado/feelingloadapp/services/UserAuthenticationService.java
@@ -1,7 +1,52 @@
 package edu.uoc.churtado.feelingloadapp.services;
 
+import android.support.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.concurrent.Executor;
 
 public class UserAuthenticationService {
-    private FirebaseAuth mAuth;
+    public boolean IsUserSignedIn(){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        return currentUser != null;
+    }
+
+    public void RegisterUser(String email, String password){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void SignInUser(String email, String password){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void SignOut(){
+        FirebaseAuth.getInstance().signOut();
+    }
 }
