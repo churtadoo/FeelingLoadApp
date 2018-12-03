a4b83761c8014d17f9e239507113f911c2cb0859
cristina.hurtado <cristina.hurtado@birchmangroup.com>
2018-12-03 22:51:56 +0100

    Added tabs in coach view

--- app/src/main/java/edu/uoc/churtado/feelingloadapp/activities/MainCoachActivity.java
+++ app/src/main/java/edu/uoc/churtado/feelingloadapp/activities/MainCoachActivity.java
@@ -1,9 +1,14 @@
 package edu.uoc.churtado.feelingloadapp.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import edu.uoc.churtado.feelingloadapp.R;
+import edu.uoc.churtado.feelingloadapp.adapters.ViewPagerAdapter;
+import edu.uoc.churtado.feelingloadapp.fragments.PlayersFragment;
+import edu.uoc.churtado.feelingloadapp.fragments.TrainingsFragment;
 
 public class MainCoachActivity extends AppCompatActivity {
 
@@ -11,5 +16,16 @@ public class MainCoachActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_coach);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add Fragments to adapter one by one
+        adapter.addFragment(new PlayersFragment(), "PLAYERS");
+        adapter.addFragment(new TrainingsFragment(), "TRAININGS");
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/edu/uoc/churtado/feelingloadapp/fragments/PlayersFragment.java
@@ -0,0 +1,27 @@
+package edu.uoc.churtado.feelingloadapp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uoc.churtado.feelingloadapp.R;
+
+public class PlayersFragment extends Fragment {
+    public PlayersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.players_fragment, container, false);
+    }
+}
